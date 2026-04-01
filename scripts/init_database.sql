757658df6094e97b3e05b870e5221e80073dcce3
tpgx404 <thanmayipgowda@gmail.com>
2026-04-01 09:35:39 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -22,3 +22,20 @@ BEGIN
     DROP DATABASE DataWarehouse;
 END;
 GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
