7891b6b472811d4268508cdcddb2d4f0ac518392
tpgx404 <thanmayipgowda@gmail.com>
2026-04-03 11:03:58 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,1 +1,10 @@
 
+CREATE TABLE bronze.crm_cust_info (
+cst_id INT ,
+cst_key NVARCHAR(50) ,
+cst_firstname NVARCHAR(50) ,
+cst_lastname NVARCHAR(50) ,
+cst_marital_status NVARCHAR(50) ,
+cst_gndr NVARCHAR(50) ,
+cst_create_date DATE
+);
