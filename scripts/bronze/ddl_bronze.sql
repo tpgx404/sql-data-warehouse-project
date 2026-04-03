95668dd8e5390871ea57c7dafd4f888e01a98ccc
tpgx404 <thanmayipgowda@gmail.com>
2026-04-03 11:22:07 +0530

    Add new table 'erp_px_cat_g1v2' to ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -40,3 +40,10 @@ cid   NVARCHAR(50) ,
 bdate DATE ,
 gen   NVARCHAR(50) 
 ) ;
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+id          NVARCHAR(50) ,
+cat         NVARCHAR(50) ,
+subcat      NVARCHAR(50) ,
+maintenance NVARCHAR(50)
+);
