0c5b3bc4f6f94ca93154d21ddf889a591cf54a5f
tpgx404 <thanmayipgowda@gmail.com>
2026-04-03 11:04:37 +0530

    Add crm_prd_info table to ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -8,3 +8,13 @@ cst_marital_status NVARCHAR(50) ,
 cst_gndr NVARCHAR(50) ,
 cst_create_date DATE
 );
+
+CREATE TABLE bronze.crm_prd_info (
+prd_id INT ,
+prd_key NVARCHAR(50) ,
+prd_nm NVARCHAR(50) ,
+prd_cost INT ,
+prd_line NVARCHAR(50) ,
+prd_start_dt DATETIME ,
+prd_end_dt DATETIME
+) ;
