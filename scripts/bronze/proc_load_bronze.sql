e01b9ed73d739ace81525e4bc3b9edfba1e6aa73
tpgx404 <thanmayipgowda@gmail.com>
2026-04-05 11:20:16 +0530

    Add bulk insert operations for multiple tables

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,1 +1,63 @@
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\dwh-project\cust_info.csv'
+WITH (
+     FIRSTROW = 2,
+     FIELDTERMINATOR = ',' ,
+     TABLOCK
+) ;
 
+SELECT COUNT(*) FROM bronze.crm_cust_info ;  /* quality check */
+
+TRUNCATE TABLE bronze.crm_cust_info ;
+BULK INSERT bronze.crm_cust_info
+FROM 'C:\dwh-project\cust_info.csv'
+WITH (
+     FIRSTROW = 2,
+     FIELDTERMINATOR = ',' ,
+     TABLOCK
+) ;
+
+TRUNCATE TABLE bronze.crm_prd_info ;
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\dwh-project\prd_info.csv'
+WITH (
+     FIRSTROW = 2,
+     FIELDTERMINATOR = ',' ,
+     TABLOCK
+) ;
+
+TRUNCATE TABLE bronze.crm_sales_details ;
+BULK INSERT bronze.crm_sales_details
+FROM 'C:\dwh-project\sales_details.csv'
+WITH (
+     FIRSTROW = 2,
+     FIELDTERMINATOR = ',' ,
+     TABLOCK
+) ;
+
+TRUNCATE TABLE bronze.erp_loc_a101 ;
+BULK INSERT bronze.erp_loc_a101
+FROM 'C:\dwh-project\LOC_A101.csv'
+WITH (
+     FIRSTROW = 2,
+     FIELDTERMINATOR = ',' ,
+     TABLOCK
+) ;
+
+TRUNCATE TABLE bronze.erp_cust_az12 ;
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\dwh-project\CUST_AZ12.csv'
+WITH (
+     FIRSTROW = 2,
+     FIELDTERMINATOR = ',' ,
+     TABLOCK
+) ;
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2 ;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\dwh-project\PX_CAT_G1V2.csv'
+WITH (
+     FIRSTROW = 2,
+     FIELDTERMINATOR = ',' ,
+     TABLOCK
+) ;
