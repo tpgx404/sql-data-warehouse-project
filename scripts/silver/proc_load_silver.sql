e0b9a12dae57a57bdc2afb4adcf8af06c6a47db9
tpgx404 <190847617+tpgx404@users.noreply.github.com>
2026-04-21 10:08:57 +0530

    Insert customer info into crm_cust_info table

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,1 +1,27 @@
-
+INSERT INTO silver.crm_cust_info (
+  cst_id ,
+  cst_key ,
+  cst_firstname ,
+  cst_lastname ,
+  cst_marital_status ,
+  cst_gndr ,
+  cst_create_date 
+)
+SELECT 
+ cst_id , 
+ cst_key ,
+ TRIM (cst_firstname) AS cst_firstname ,
+ TRIM ( cst_lastname) AS cst_lastname ,
+ cst_marital_status ,
+ cst_gndr ,
+ CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+      WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+ ELSE 'n/a'
+ END cst_marital_status ,
+ cst_create_date 
+ FROM ( SELECT 
+         * ,
+         ROW_NUMBER () OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC ) AS flag_last
+         FROM bronze.crm_cust_info 
+         WHERE cst_id IS NOT NULL
+         ) t WHERE flag_last = 1 ;
